88c12e47907f7160505fedb1ff0487241d8549a5
first19326 <870390039@qq.com>
2023-05-12 14:51:17 +0800

    update icon src

--- vite.config.js
+++ vite.config.js
@@ -60,31 +60,31 @@ export default ({
                 "theme_color": "#424242",
                 "background_color": "#424242",
                 "icons": [{
-                    "src": "/images/icon/48.png",
+                    "src": "/image/icon/48.png",
                     "sizes": "48x48",
                     "type": "image/png"
                 }, {
-                    "src": "/images/icon/72.png",
+                    "src": "/image/icon/72.png",
                     "sizes": "72x72",
                     "type": "image/png"
                 }, {
-                    "src": "/images/icon/96.png",
+                    "src": "/image/icon/96.png",
                     "sizes": "96x96",
                     "type": "image/png"
                 }, {
-                    "src": "/images/icon/128.png",
+                    "src": "/image/icon/128.png",
                     "sizes": "128x128",
                     "type": "image/png"
                 }, {
-                    "src": "/images/icon/144.png",
+                    "src": "/image/icon/144.png",
                     "sizes": "144x144",
                     "type": "image/png"
                 }, {
-                    "src": "/images/icon/192.png",
+                    "src": "/image/icon/192.png",
                     "sizes": "192x192",
                     "type": "image/png"
                 }, {
-                    "src": "/images/icon/512.png",
+                    "src": "/image/icon/512.png",
                     "sizes": "512x512",
                     "type": "image/png"
                 }]
