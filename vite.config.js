4bd92724ce6daa8696896159ce51479fced7181a
first19326 <870390039@qq.com>
2023-05-28 03:27:04 +0800

    add 256x256 icon

--- vite.config.js
+++ vite.config.js
@@ -84,6 +84,10 @@ export default ({
                     "sizes": "192x192",
                     "type": "image/png"
                 }, {
+                    "src": "/image/icon/256.png",
+                    "sizes": "256x256",
+                    "type": "image/png"
+                }, {
                     "src": "/image/icon/512.png",
                     "sizes": "512x512",
                     "type": "image/png"
