f562c9981ddf452fbe6e40e3c704b853efc4486b
first19326 <870390039@qq.com>
2023-11-05 00:15:05 +0800

    update the introduction method of environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,22 @@
 import { defineConfig, loadEnv } from 'vite';
+import { resolve } from 'path';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { createHtmlPlugin } from 'vite-plugin-html';
-import { resolve } from 'path';
-import { VitePWA } from 'vite-plugin-pwa';
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
-export default ({
-    mode
-}) => defineConfig({
+export default ({ mode }) => defineConfig({
     plugins: [
         vue(),
         AutoImport({
             imports: ["vue"],
+            resolvers: [ElementPlusResolver()]
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-        createHtmlPlugin({
-            minify: true,
-            template: 'index.html',
-            inject: {
-                data: {
-                    title: loadEnv(mode, process.cwd()).VITE_SITE_NAME,
-                    author: loadEnv(mode, process.cwd()).VITE_SITE_AUTHOR,
-                    keywords: loadEnv(mode, process.cwd()).VITE_SITE_KEYWORDS,
-                    description: loadEnv(mode, process.cwd()).VITE_SITE_DES,
-                    icon: loadEnv(mode, process.cwd()).VITE_SITE_ICON,
-                    icon_apple: loadEnv(mode, process.cwd()).VITE_SITE_ICON_APPLE,
-                    count: loadEnv(mode, process.cwd()).VITE_SITE_BAIDUCOUNT,
-                },
-            },
+            resolvers: [ElementPlusResolver()]
         }),
         VitePWA({
             registerType: 'autoUpdate',
@@ -43,68 +27,68 @@ export default ({
                     urlPattern: /(.*?)\.(js|css|woff2|woff|ttf)/, // js / css 静态资源缓存
                     handler: 'CacheFirst',
                     options: {
-                        cacheName: 'js-css-cache',
-                    },
+                        cacheName: 'js-css-cache'
+                    }
                 }, {
                     urlPattern: /(.*?)\.(png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/, // 图片缓存
                     handler: 'CacheFirst',
                     options: {
-                        cacheName: 'image-cache',
-                    },
-                }],
+                        cacheName: 'image-cache'
+                    }
+                }]
             },
             manifest: {
-                "name": loadEnv(mode, process.cwd()).VITE_SITE_NAME,
-                "short_name": loadEnv(mode, process.cwd()).VITE_SITE_NAME,
-                "description": loadEnv(mode, process.cwd()).VITE_SITE_DES,
-                "display": "standalone",
-                "start_url": "/",
-                "theme_color": "#424242",
-                "background_color": "#424242",
-                "icons": [{
-                    "src": "/image/icon/48.png",
-                    "sizes": "48x48",
-                    "type": "image/png"
+                name: loadEnv(mode, process.cwd()).VITE_SITE_NAME,
+                short_name: loadEnv(mode, process.cwd()).VITE_SITE_NAME,
+                description: loadEnv(mode, process.cwd()).VITE_SITE_DES,
+                display: "standalone",
+                start_url: "/",
+                theme_color: "#424242",
+                background_color: "#424242",
+                icons: [{
+                    src: "/image/icon/48.png",
+                    sizes: "48x48",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/72.png",
-                    "sizes": "72x72",
-                    "type": "image/png"
+                    src: "/image/icon/72.png",
+                    sizes: "72x72",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/96.png",
-                    "sizes": "96x96",
-                    "type": "image/png"
+                    src: "/image/icon/96.png",
+                    sizes: "96x96",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/128.png",
-                    "sizes": "128x128",
-                    "type": "image/png"
+                    src: "/image/icon/128.png",
+                    sizes: "128x128",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/144.png",
-                    "sizes": "144x144",
-                    "type": "image/png"
+                    src: "/image/icon/144.png",
+                    sizes: "144x144",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/192.png",
-                    "sizes": "192x192",
-                    "type": "image/png"
+                    src: "/image/icon/192.png",
+                    sizes: "192x192",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/256.png",
-                    "sizes": "256x256",
-                    "type": "image/png"
+                    src: "/image/icon/256.png",
+                    sizes: "256x256",
+                    type: "image/png"
                 }, {
-                    "src": "/image/icon/512.png",
-                    "sizes": "512x512",
-                    "type": "image/png"
+                    src: "/image/icon/512.png",
+                    sizes: "512x512",
+                    type: "image/png"
                 }]
             }
         }),
+        viteCompression()
     ],
     server: {
-        port: "3000",
-        hmr: true,
+        port: "3000"
     },
     resolve: {
         alias: [{
             find: '@',
-            replacement: resolve(__dirname, "src"),
+            replacement: resolve(__dirname, "src")
         }]
     },
     css: {
@@ -120,7 +104,7 @@ export default ({
         terserOptions: {
             compress: {
                 // 生产环境时移除 console
-                pure_funcs: ['console.log'],
+                pure_funcs: ['console.log']
             }
         }
     }
